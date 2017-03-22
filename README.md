fd78b5fc411144826881ecc818b925e73b61b73a
carlosfdev <carlosfdev@gmail.com>
2017-03-23 00:11:30 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# socialfleet
+Project from the "Two Tier Enterprise App and API Development with Angular &amp; Sails" Pluralsight's course 
---
+++ frontend/gulpfile.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp');
+var webServer = require('gulp-webserver');
+var mainBowerFiles = require('main-bower-files');
+var inject = require('gulp-inject');
+var del = require('del');
+
+var paths = {
+    temp: 'temp',
+    tempVendor: 'temp/vendor',
+    tempIndex: 'temp/index.html',
+
+    index: 'app/index.html',
+    appSrc: ['app/**/*', '!app/index.html'],
+    bowerSrc: 'bower_components'
+};
+
+gulp.task('default', ['watch']);
+
+
+
+gulp.task('watch', ['serve'], function() {
+    gulp.watch(paths.appSrc, ['copyAndInjectScripts']);
+    gulp.watch(paths.bowerSrc, ['copyAndInjectVendors']);
+    gulp.watch(paths.index, ['copyAndInjectAll']);
+});
+
+gulp.task('serve', function() {
+    gulp.src(paths.temp)
+        .pipe(webServer({
+            livereload: true,
+            proxies: [{
+                source: '/api',
+                target: 'http://localhost:1337'
+            }]
+        }));
+});
+
+gulp.task('copyAndInjectAll', function() {
+    var tempVendors = gulp.src(mainBowerFiles()).pipe(gulp.dest(paths.tempVendor));
+    var appFiles = gulp.src(paths.appSrc).pipe(gulp.dest(paths.temp));
+
+    return gulp.src(paths.index)
+        .pipe(inject(tempVendors, {
+            relative: true,
+            name: 'vendorInject'
+        }))
+        .pipe(inject(appFiles, {
+            relative: true
+        }))
+        .pipe(gulp.dest(paths.temp));
+});
+
+gulp.task('copyAndInjectVendors', function() {
+    var tempVendors = gulp.src(mainBowerFiles()).pipe(gulp.dest(paths.tempVendor));
+
+    return gulp.src(paths.tempIndex)
+        .pipe(inject(tempVendors, {
+            relative: true,
+            name: 'vendorInject'
+        }))
+        .pipe(gulp.dest(paths.temp));
+});
+
+gulp.task('copyAndInjectScripts', function() {
+    var appFiles = gulp.src(paths.appSrc).pipe(gulp.dest(paths.temp));
+
+    return gulp.src(paths.tempIndex)
+        .pipe(inject(appFiles, {
+            relative: true
+        }))
+        .pipe(gulp.dest(paths.temp));
+});
+
+gulp.task('clean', function() {
+    del([paths.temp]);
+});
